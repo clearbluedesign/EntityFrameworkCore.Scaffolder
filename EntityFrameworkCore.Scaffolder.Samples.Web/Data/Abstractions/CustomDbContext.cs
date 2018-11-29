8acfebdc1f5254305c6f077a485633f21b5ac46e
Roman Popovici <lambidu@live.com>
2018-11-29 02:12:39 +0200

    Add: DbSets to CustomDbContext

--- EntityFrameworkCore.Scaffolder.Samples.Web/Data/Abstractions/CustomDbContext.cs
+++ EntityFrameworkCore.Scaffolder.Samples.Web/Data/Abstractions/CustomDbContext.cs
@@ -1,9 +1,15 @@
+using ClearBlueDesign.EntityFrameworkCore.Scaffolder.Samples.Web.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 
 
 namespace ClearBlueDesign.EntityFrameworkCore.Scaffolder.Samples.Web.Data.Abstractions {
 	public abstract class CustomDbContext<T> : DbContext where T : DbContext {
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+
+
 		public CustomDbContext() {
 
 		}
